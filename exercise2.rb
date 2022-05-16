fa3f254d10924fb86176bc51ed1d23702e27b36e
morookaS <v21e3020@oita-u.ac.jp>
2022-05-16 04:53:05 +0000

    Lesson03課題

---
+++ exercise2.rb
@@ -0,0 +1,14 @@
+str1 = "test"
+p str1
+str2 = "test"
+p str2
+str3 = "tezt"
+p str3
+
+#文字列の比較　trur or false
+p str1 == str2
+p str1 == str3
+
+#文字列を,で分割
+string = "AAA, BBB, CCC"
+p string.split(",")
